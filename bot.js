fe3e7a8516b30bc867e3cd292b0e9761d8a17fdc
IS0metric <swgbhero@ymail.com>
2018-02-26 16:30:02 +0000

    Added extra catches and space removal

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const bot_message = "`If you are considering revolting against our Admin, I would be very careful. Ze sees all. Unless it's the feminist revolution in which case, go ahead.`";
-const catches = ["revolt", "revolu", "rev0lt", "rev0lu", "r3volt", "r3volu", "r3v0lt", "r3v0lu", "revoit", "revoiu"];
+const catches = ["revolt", "revolu", "rev0lt", "rev0lu", "r3volt", "r3volu", "r3v0lt", "r3v0lu", "revoit", "revoiu", "revlt", "revo1t", "revo1u", "rev01t", "rev01u", "r3vo1t", "r3vo1u", "r3v01t", "r3v01u"];
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -10,7 +10,7 @@ client.on("ready", () => {
 client.on("message", (message) => {
   if (message.author.bot) return;
   else {
-    var stripped_message = message.content.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var stripped_message = message.content.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]\s/g,"");
     catches.forEach(function(word) {
       if (stripped_message.includes(word)) {
         console.log(word);
